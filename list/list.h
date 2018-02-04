1e8592b6dc1d8b6ad9141cea48c6a2ae23c9112e
Michael Glatzmaier <michael.glatzmaier@gmail.com>
2018-02-04 10:18:01 -0600

    Start linked list algorithms.

---
+++ list/list.h
@@ -0,0 +1,15 @@
+#ifndef LIST_H
+#define LIST_H
+
+template<typename T> 
+struct Node {
+  Node * next;
+  T data;
+};
+
+namespace List {
+
+
+};
+
+#endif
---
+++ list/main.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <cstring>
+#include <vector>
+
+#include "threads.h"
+#include "rand.h"
+#include "clock.h"
+#include "list.h"
+
+void test_linked_list();
+
+int main(int argc, char ** argv) {
+
+  return 0;
+}
+
+void test_linked_list() { 
+
+}
